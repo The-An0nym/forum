d2233761194ec85c0f545d1d6b68daf60c64e43d
The-An0nym <114821295+The-An0nym@users.noreply.github.com>
2025-05-02 17:25:47 +0200

    Avoiding function re-decleration

--- src/functions/.connect.php
+++ src/functions/.connect.php
@@ -1,12 +1,14 @@
 <?php
-function getConn () {
-    static $conn;
-    if ($conn===NULL){ 
-        $path = $_SERVER['DOCUMENT_ROOT'];
-        $configs = include($path . '/functions/.config.php');
-        extract($configs);
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!function_exists('getConn')) {
+    function getConn () {
+        static $conn;
+        if ($conn===NULL){ 
+            $path = $_SERVER['DOCUMENT_ROOT'];
+            $configs = include($path . '/functions/.config.php');
+            extract($configs);
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+        }
+        return $conn;
     }
-    return $conn;
 }
 ?>
